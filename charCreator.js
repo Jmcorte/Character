f704da3bcbe614550c34fe687f26aa80820024c1
6exf <6exf@fresnou.org>
2023-04-19 13:55:29 -0700

    updated char creator

--- charCreator.js
+++ charCreator.js
@@ -1,6 +1,7 @@
 const readLine = require('readline-sync');
 
 let diceRolled = [];
+let eyeColor = ['blue','green','brown',];
 let character1 = {
     //empty for now
 };
@@ -29,12 +30,18 @@ function sum(list){
 }
 function rollStat(list){
     list = [];
-    rollDice(4, list);
+    rollDice(5, list);
     return sum(list)
+}
+
+function pickRandomChoice(choices) {
+    let randomNum = Math.floor(Math.random()*choices.length)
+    return choices[randomNum]
 }
 
 function createChar(list, character){
     character.name = readLine.question("Enter Character Name: ");
+    character.eyeColor = pickRandomChoice(eyeColor);
     console.log('Rolling Strength')
     character.strength= rollStat(diceRolled);
     console.log('Rolling Dexterity')
@@ -47,15 +54,18 @@ function createChar(list, character){
     character.wisdom= rollStat(diceRolled);
     console.log('Rolling Charisma')
     character.charisma= rollStat(diceRolled);
+    character.luck = rollStat(diceRolled);
     displayChar(character);
 }
 
 function displayChar(char){
     console.log(`Name: ${char.name}`);
+    console.log(`Eye Color: ${char.eyeColor}`);
     console.log(`STR: ${char.strength}`);
     console.log(`DEX: ${char.dexterity}`);
     console.log(`CON: ${char.constitution}`);
     console.log(`INT: ${char.intelligence}`);
     console.log(`WIS: ${char.wisdom}`);
     console.log(`CHA: ${char.charisma}`);
+    console.log(`LCK: ${char.luck}`);
 }
