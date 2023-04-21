bdad2e579a7eed68453b68ae56b091706adf9c36
6exf <6exf@fresnou.org>
2023-04-21 14:18:54 -0700

    updated char creator

---
+++ combomenu.js
@@ -0,0 +1,4 @@
+const readLine= require('readline-sync');
+let menu=[`Chicken Sandwich 3$:`,'Beef Sandwich 2$: ','Tofu Sandwich 1.']
+let bevs = ['Pepsi 1$','Squirt 1.25$','Coca Cola 1$',];
+let fries =['Small','Medium','Large','Xtra Large'];
